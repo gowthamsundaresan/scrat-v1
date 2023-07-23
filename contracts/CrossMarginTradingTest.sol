3a938fe5775033bf1fc82e8026abb353405ce7de
Gowtham S <gowtham54321@yahoo.com>
2023-07-23 17:23:57 -0400

    first commit

--- contracts/CrossMarginTradingTest.sol
+++ contracts/CrossMarginTradingTest.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
+import "hardhat/console.sol";
+
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
@@ -9,7 +11,7 @@ import "@aave/core-v3/contracts/interfaces/IPool.sol";
 import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
 
-contract CrossMarginTradingTest is IFlashLoanRecipient {
+contract TestingCrossMarginTrading is IFlashLoanRecipient {
     // Token Addresses
     address private constant usdcTokenAddress = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
     address private constant wmaticTokenAddress = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
@@ -67,15 +69,20 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
         // Instantiate Aave V3 Pool
         iPoolAddress = i_poolAddressesProvider.getPool();
         aavePool = IPool(iPoolAddress);
+        console.log("Pool instantiated with address: ");
+        console.log(address(aavePool));
 
         // Transfer USDC to contract
         i_usdc.transferFrom(msg.sender, address(this), amount);
+        console.log("Amount Transferred...");
 
         // Approve Pool contract to spend USDC
         i_usdc.approve(iPoolAddress, amount);
+        console.log("Amount Approved...");
 
         // Supply all USDC to Aave V3 on behalf of user
         aavePool.supply(address(i_usdc), amount, msg.sender, 0);
+        console.log("Supplied to Aave...");
     }
 
     /**
@@ -106,6 +113,8 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
         uint256[] memory /* feeAmounts */,
         bytes memory userData
     ) external override {
+        console.log("Flashloan request received...");
+
         // Parse data
         IERC20 token = tokens[0];
         uint256 amount = amounts[0];
@@ -114,19 +123,32 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
             (address, address, uint8, uint256)
         );
 
+        uint256 tokenBalance = IERC20(tokenAddress).balanceOf(address(this));
+        console.log("This contract now holds token balance of: ");
+        console.log(tokenBalance);
+
+        uint256 usdcBalance = i_usdc.balanceOf(address(this));
+        console.log("This contract now holds USDC of: ");
+        console.log(usdcBalance);
+
         // Perform intended action
         if (direction == 0) {
+            console.log("Open long position request received...");
             openLongPosition(amount, tokenAddress, userAddress);
         } else if (direction == 1) {
+            console.log("Open short position request received...");
             openShortPosition(amount, tokenAddress, userAddress);
         } else if (direction == 2) {
+            console.log("Close long position request received...");
             closeLongPosition(amount, tokenAddress, userAddress, profit);
         } else if (direction == 3) {
+            console.log("Close short position request received...");
             closeShortPosition(amount, tokenAddress, userAddress, profit);
         }
 
         // Repay flashloan
         token.transfer(address(i_vault), amount);
+        console.log("Flashloan has been repayed...");
     }
 
     /**
@@ -138,6 +160,7 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
      * @param direction 0 = long, 1 = short
      */
     function requestOpen(uint256 amount, address tokenAddress, uint8 direction) public {
+        console.log("Entered function requestOpen()...");
         address userAddress = msg.sender;
 
         // Setup flashloan request
@@ -147,6 +170,7 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
 
         direction == 0 ? tokens[0] = i_usdc : tokens[0] = IERC20(tokenAddress);
         amounts[0] = amount;
+        console.log("Flashloan has been setup...");
 
         // Request flashloan
         i_vault.flashLoan(this, tokens, amounts, userData);
@@ -190,6 +214,7 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
     function openShortPosition(uint256 amount, address tokenAddress, address userAddress) private {
         // Approve Uni V3 router to spend USDC
         IERC20(tokenAddress).approve(address(i_swapRouter), amount);
+        console.log("Swaprouter approved for ", amount);
 
         // Swap requested asset for USDC on Uni V3
         ISwapRouter.ExactInputSingleParams memory inputParams = ISwapRouter.ExactInputSingleParams(
@@ -210,16 +235,20 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
             0
         );
         i_swapRouter.exactInputSingle(inputParams);
+        console.log("Swapped ", amount, "for USDC ", quotedAmountBase);
 
         // Approve Aave Pool contract to spend token
         IERC20 token = IERC20(address(i_usdc));
         token.approve(iPoolAddress, quotedAmountBase);
+        console.log("Approved Aave for USDC ", quotedAmountBase);
 
         // Supply USDC on Aave V3
         aavePool.supply(address(i_usdc), quotedAmountBase, userAddress, 0);
+        console.log("Supplied on Aave: USDC ", quotedAmountBase);
 
         // Borrow requested asset on Aave v3
         aavePool.borrow(tokenAddress, amount, 2, 0, userAddress);
+        console.log("Borrowing on Aave: ", amount);
     }
 
     /**
@@ -238,6 +267,7 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
         uint8 direction,
         uint256 profit
     ) public {
+        console.log("Entered function requestClose()...");
         address userAddress = msg.sender;
         IERC20[] memory tokens = new IERC20[](1);
         uint256[] memory amounts = new uint256[](1);
@@ -246,6 +276,7 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
         direction == 2 ? tokens[0] = IERC20(tokenAddress) : tokens[0] = i_usdc;
         amounts[0] = repay;
 
+        console.log("Requesting flashloan...");
         i_vault.flashLoan(this, tokens, amounts, userData);
     }
 
@@ -255,11 +286,18 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
         address userAddress,
         uint256 profit
     ) private {
+        console.log("Entered closeLongPosition()...");
         uint256 totalPosition = repay + profit;
+        uint256 tcb;
+        uint256 tdb;
+
+        console.log("Total Position: ");
+        console.log(totalPosition);
 
         // Approve Uni V3 router to spend requested asset
         IERC20 token = IERC20(tokenAddress);
         token.approve(address(i_swapRouter), totalPosition);
+        console.log("Uni V3 approved...");
 
         // Swap requested asset for USDC on Uni V3
         ISwapRouter.ExactInputSingleParams memory inputParams = ISwapRouter.ExactInputSingleParams(
@@ -280,25 +318,64 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
             0
         );
         i_swapRouter.exactInputSingle(inputParams);
+        console.log("Swap successful...");
+        console.log(quotedAmountBase);
 
         // Approve Aave Pool contract to spend USDC
         i_usdc.approve(iPoolAddress, quotedAmountBase);
+        console.log("Aave approved for: ");
+        console.log(quotedAmountBase);
+
+        // Check Aave
+        (tcb, tdb, , , , ) = aavePool.getUserAccountData(userAddress);
+        console.log("Current TCB: ");
+        console.log(tcb);
+        console.log("Current TDB: ");
+        console.log(tdb);
 
         // Repay quotedAmountBase (USDC) debt on Aave V3
         aavePool.repay(address(i_usdc), quotedAmountBase, 2, userAddress);
+        console.log("Repaid debt on Aave");
+
+        // Check Aave
+        (tcb, tdb, , , , ) = aavePool.getUserAccountData(userAddress);
+        console.log("Current TCB: ");
+        console.log(tcb);
+        console.log("Current TDB: ");
+        console.log(tdb);
 
         // Transfer aTokens of requested asset to contract
         IERC20 aToken = IERC20(tokenAddressToATokenAddress[tokenAddress]);
+        console.log("User aToken balance: ");
+        console.log(aToken.balanceOf(userAddress));
         aToken.transferFrom(userAddress, address(this), totalPosition);
+        console.log("Received aTokens from user");
+        console.log("Balance of aTokens: ");
+        console.log(aToken.balanceOf(address(this)));
 
         // Withdraw requested asset on Aave V3
         aavePool.withdraw(tokenAddress, aToken.balanceOf(address(this)), userAddress);
+        console.log("Withdrew tokens from Aave");
+
+        // Check Aave
+        (tcb, tdb, , , , ) = aavePool.getUserAccountData(userAddress);
+        console.log("Current TCB: ");
+        console.log(tcb);
+        console.log("Current TDB: ");
+        console.log(tdb);
 
         // Transfer newly withdrawn tokens of requested asset to contract
         token.transferFrom(userAddress, address(this), totalPosition);
+        console.log("Received tokens from user");
+        console.log("Balance of token: ");
+        console.log(token.balanceOf(address(this)));
 
         if (profit > 0) {
+            console.log("This trade is profitable...");
+
             // Swap profit for USDC
+            console.log("Amount to swap on Uni V3: ");
+            console.log(profit);
             inputParams = ISwapRouter.ExactInputSingleParams(
                 tokenAddress,
                 address(i_usdc),
@@ -317,12 +394,17 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
                 0
             );
             i_swapRouter.exactInputSingle(inputParams);
+            console.log("Swapped profit on Uniswap");
+            console.log(quotedAmountBase);
 
             // Approve Aave Pool contract to spend USDC
             i_usdc.approve(iPoolAddress, quotedAmountBase);
+            console.log("Aave approved for: ");
+            console.log(quotedAmountBase);
 
             // Supply profit in USDC back into Aave V3
             aavePool.supply(address(i_usdc), quotedAmountBase, userAddress, 0);
+            console.log("Profit supplied back to Aave: ");
         }
     }
 
@@ -332,10 +414,17 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
         address userAddress,
         uint256 profit
     ) private {
+        console.log("Entered closeShortPosition()...");
         uint256 totalPosition = repay + profit;
+        uint256 tcb;
+        uint256 tdb;
+
+        console.log("Total Position: ");
+        console.log(totalPosition);
 
         // Approve Uni V3 router to spend USDC
         i_usdc.approve(address(i_swapRouter), totalPosition);
+        console.log("Uni V3 approved...");
 
         // Swap requested asset for USDC on Uni V3
         ISwapRouter.ExactInputSingleParams memory inputParams = ISwapRouter.ExactInputSingleParams(
@@ -356,30 +445,70 @@ contract CrossMarginTradingTest is IFlashLoanRecipient {
             0
         );
         i_swapRouter.exactInputSingle(inputParams);
+        console.log("Swap successful...");
+        console.log(quotedAmountBase);
 
         // Approve Aave Pool contract to spend requested asset
         IERC20 token = IERC20(tokenAddress);
         token.approve(iPoolAddress, quotedAmountBase);
+        console.log("Aave approved for: ");
+        console.log(quotedAmountBase);
+
+        // Check Aave
+        (tcb, tdb, , , , ) = aavePool.getUserAccountData(userAddress);
+        console.log("Current TCB: ");
+        console.log(tcb);
+        console.log("Current TDB: ");
+        console.log(tdb);
 
         // Repay quotedAmountBase (requested asset) debt on Aave V3
         aavePool.repay(tokenAddress, quotedAmountBase, 2, userAddress);
+        console.log("Repaid debt on Aave");
+
+        // Check Aave
+        (tcb, tdb, , , , ) = aavePool.getUserAccountData(userAddress);
+        console.log("Current TCB: ");
+        console.log(tcb);
+        console.log("Current TDB: ");
+        console.log(tdb);
 
         // Transfer aTokens of USDC to contract
         IERC20 aToken = IERC20(tokenAddressToATokenAddress[address(i_usdc)]);
+        console.log("User aToken balance: ");
+        console.log(aToken.balanceOf(userAddress));
         aToken.transferFrom(userAddress, address(this), totalPosition);
+        console.log("Received aTokens from user");
+        console.log("Balance of aTokens: ");
+        console.log(aToken.balanceOf(address(this)));
 
         // Withdraw USDC on Aave V3
         aavePool.withdraw(address(i_usdc), aToken.balanceOf(address(this)), userAddress);
+        console.log("Withdrew USDC from Aave");
+
+        // Check Aave
+        (tcb, tdb, , , , ) = aavePool.getUserAccountData(userAddress);
+        console.log("Current TCB: ");
+        console.log(tcb);
+        console.log("Current TDB: ");
+        console.log(tdb);
 
         // Transfer newly withdrawn USDC tokens to contract
         i_usdc.transferFrom(userAddress, address(this), totalPosition);
+        console.log("Received USDC from user");
+        console.log("Balance of USDC: ");
+        console.log(i_usdc.balanceOf(address(this)));
 
         if (profit > 0) {
+            console.log("This trade is profitable...");
+
             // Approve Aave Pool contract to spend USDC
             i_usdc.approve(iPoolAddress, profit);
+            console.log("Aave approved for: ");
+            console.log(profit);
 
             // Supply profit in USDC back into Aave V3
             aavePool.supply(address(i_usdc), profit, userAddress, 0);
+            console.log("Profit supplied back to Aave: ");
         }
     }
 
